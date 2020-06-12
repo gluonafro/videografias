a3db8875b9b254f964e5fb121734318a6d3529fe
glopezi <glopezi@minsait.com>
2020-06-12 22:06:28 +0200

    Updates

---
+++ precache-manifest.5612fd25dae15478bdbd6f75c0f7c13d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49edbc94338606f7a61219060ec15b89",
+    "url": "/videografias/index.html"
+  },
+  {
+    "revision": "da51a47aec40f1aeecae",
+    "url": "/videografias/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5f0dfa8e443fb1b97ac1",
+    "url": "/videografias/static/js/2.33cd2b55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/videografias/static/js/2.33cd2b55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da51a47aec40f1aeecae",
+    "url": "/videografias/static/js/main.325fe42b.chunk.js"
+  },
+  {
+    "revision": "d92700ed85e2346d716b",
+    "url": "/videografias/static/js/runtime-main.039dbf35.js"
+  }
+]);
