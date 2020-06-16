08537fd7eb8c9b68b53924cb4369393ab82c1bb4
glopezi <glopezi@minsait.com>
2020-06-16 20:39:45 +0200

    comments forcie11 development

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import "react-app-polyfill/ie11";
 import "react-app-polyfill/stable";
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+//fixed IE11 in development mode,
+//delete after https://github.com/facebook/create-react-app/issues/8197 is solved and we update
+// if (process.env.NODE_ENV === "development") {
+//   require("fast-text-encoding/text");
+// }
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
