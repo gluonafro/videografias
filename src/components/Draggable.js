44ffe8a287fcc5ce690ee20fbc4ea1d7f58088ec
gluonafro <gluonafro@outlook.es>
2020-06-21 12:31:01 +0200

    dragging prueba

--- src/components/Draggable.js
+++ src/components/Draggable.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from "react";
+import usePrevious from "../hooks/usePrevious"
 
 const Draggable = ({ initialPos = 0 }) => {
   const [pos, setPos] = useState(initialPos);
@@ -7,51 +8,71 @@ const Draggable = ({ initialPos = 0 }) => {
 
   const Bola = useRef();
 
+  let prevDragging = usePrevious(dragging)
+
+    // calculate relative position to the mouse and set dragging=true
+    const onMouseDown = (e) => {
+      if (e.button !== 0) return; // only left mouse button
+      console.log("onmouseDOWN: drag -> " + dragging);
+      console.log(e.pageX);
+      var pos = Bola.current.offsetLeft;
+      console.log(pos);
+      setDragging(true);
+      setRel(e.pageX - pos);
+      e.stopPropagation();
+      e.preventDefault();
+    };
+    const onMouseUp = (e) => {
+      console.log("onmouseUP: drag -> " + dragging);
+      setDragging(false);
+      // console.log(dragging)
+      e.stopPropagation();
+      e.preventDefault();
+    };
+    const onMouseMove = (e) => {
+      console.log('mousemove: drag -> ' + dragging)
+      if (!dragging) return;
+      setPos(e.pageX - rel);
+      e.stopPropagation();
+      e.preventDefault();
+    };
+  
+
   // we could get away with not having this (and just having the listeners on
   // our div), but then the experience would be possibly be janky. If there's
   // anything w/ a higher z-index that gets in the way, then you're toast,
   // etc.
 
-  useEffect(() => {
-    if (dragging) {
-      document.addEventListener("mousemove", onMouseMove);
-      document.addEventListener("mouseup", onMouseUp);
-    } else {
-      document.removeEventListener("mousemove", onMouseMove);
-      document.removeEventListener("mouseup", onMouseUp);
-    }
-  }, [dragging]);
+  // useEffect(() => {
+  //   // console.log(dragging)
+  //   if (dragging && !prevDragging) {
+  //     document.addEventListener("mousemove", onMouseMove)
+  //     document.addEventListener("mouseup", onMouseUp);
+  //     document.removeEventListener("mousedown", onMouseDown);
+  //   } else if (!dragging && prevDragging) {
+  //     document.removeEventListener("mousemove", onMouseMove);
+  //     document.removeEventListener("mouseup", onMouseUp);
+  //   }
+  //   return () => setDragging(false)
+  // }, [dragging]);
 
-  // calculate relative position to the mouse and set dragging=true
-  const onMouseDown = (e) => {
-    if (e.button !== 0) return; // only left mouse button
-    console.log(e.pageX);
-    var pos = Bola.current.offsetLeft;
-    console.log(pos);
-    setDragging(true);
-    setRel(e.pageX - pos);
-    e.stopPropagation();
-    e.preventDefault();
-  };
-  const onMouseUp = (e) => {
-    console.log("asassasas");
-    setDragging(false);
-    e.stopPropagation();
-    e.preventDefault();
-  };
-  const onMouseMove = (e) => {
-    if (!dragging) return;
-    setPos(e.pageX - rel);
-    e.stopPropagation();
-    e.preventDefault();
-  };
-  console.log(dragging);
+  useEffect(() => {
+    console.log(dragging)
+    document.addEventListener("mouseup", onMouseUp)
+    // if (dragging) document.addEventListener("mousemove", onMouseMove)
+    if (!dragging) {
+      document.removeEventListener("mouseup", onMouseUp)
+      document.removeEventListener("mousemove", onMouseMove)
+    }
+  }, [dragging])
+
+
 
   return (
     <div
+    onMouseDown={onMouseDown}
+    onMouseMove={onMouseMove}
       ref={Bola}
-      onMouseDown={onMouseDown}
-      onMouseUp={onMouseUp}
       style={{ position: "absolute", left: pos + "px" }}
     ></div>
   );
