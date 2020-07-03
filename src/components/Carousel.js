06b6ea833fee3518015ee313108f95ea260c4fe7
gluonafro <gluonafro@outlook.es>
2020-07-03 19:45:19 +0200

    code cleanup

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,48 +3,28 @@ import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import styled from "styled-components";
 import ButtonGroup from "../containers/ButtonGroup";
-import {TweenMax} from 'gsap';
-import img1 from '../assets/img/01.jpg';
-import img2 from '../assets/img/02.jpg';
-import img3 from '../assets/img/03.jpg';
+import {carouselResponsive} from '../resources/constants.json'
 
-const Carrusel = () => {
-  const [zoom, setZoom] = useState(true)
-  const responsive = {
-      desktop: {
-        breakpoint: { max: 3000, min: 1024 },
-        items: 1,
-      },
-    }
+const Carrusel = props => {
   const Car = useRef(null);
-
   return (
-    <>
     <SCarousel
-      responsive={responsive}
+      responsive={carouselResponsive}
       infinite={true}
+      arrows={false}
       swipeable={false}
       ssr={false}
-      autoPlay={false}
       draggable={false}
       centerMode={true}
-      customButtonGroup={<ButtonGroup Carousel={Car} zoom={zoom} setZoom={setZoom} />}
+      customButtonGroup={<ButtonGroup Carousel={Car} {...props} />}
       itemClass={'videoClass'}
       ref={Car}
+      customTransition="all 0.4s linear"
+      transitionDuration={400}
+      autoPlaySpeed={400}
     >
-      {array.map((_, index) => (
-        <div>
-          <Video height={zoom ? '25vw' : '8vw'}>
-            <img src={index % 3 === 0 ? img1 : index % 2 === 0 ? img2 : img3} width='100%' height='100%'/>
-            </Video>
-          {/* width={zoom ? '45vw' : '15vw'}/> */}
-          <p>Video {index}</p>
-          <p>Artista {index}</p>
-          <p>1977, Argentina</p>
-        </div>
-      ))}
+      {props.children}
     </SCarousel>
-   </>
   );
 };
 
@@ -54,22 +34,6 @@ const SCarousel = styled(Carousel)`
   height: 60vh;
   padding-bottom: 120px;
   .videoClass {
-    /* width: 45vw; */
-    /* padding: 0 100px; */
   }
 `;
 
-const Video = styled.div`
-  flex-shrink: 0;
-  /* width: ${({width}) => width}; */
-  width: 80%;
-  /* height: 25vw; */
-  height: ${({height}) => height};
-  border: 1px solid #000;
-  margin: 100px 0 5px 0;
-`;
-
-let array = [];
-for (let i = 0; i < 70; i++) {
-  array.push(i);
-}
