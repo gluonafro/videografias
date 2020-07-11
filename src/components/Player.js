f215c873d4d1f0b1b922a48e0ce76e33de81f6c6
gluonafro <gluonafro@outlook.es>
2020-07-11 13:46:57 +0200

    player

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,20 +1,65 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styled from "styled-components";
+import { data } from "../resources/data.json";
+import { Link } from "react-router-dom";
 
-const Player = (props) => {
-    // const player = PlayerAPI.get(
-    //   parseInt(props.match.params.number, 10)
-    // )
-    if (!player) {
-      return <div>Sorry, but the player was not found</div>
+export default ({ match }) => {
+  return (
+    <Container>
+      <VideoInfo>
+        <button>Info video</button>
+        <button>Info artista</button>
+      </VideoInfo>
+      <VideoPlayer>
+        <video
+          src={data[match.params.id].link}
+          height="100%"
+          width="100%"
+          autoPlay={true}
+          controls
+        ></video>
+      </VideoPlayer>
+      <BackArrow>
+        <Link to="/expo">go back</Link>
+      </BackArrow>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  background: #000;
+  display: flex;
+`;
+
+const VideoInfo = styled.div`
+  height: 100%;
+  width: 100px;
+  background: #ddd;
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: column;
+  > button {
+    height: 50vh;
+    width: 100%;
+    :first-child {
+      border-bottom: 1px solid #000;
     }
-    return (
-      <div>
-        <h1>{player.name} (#{player.number})</h1>
-        <h2>Position: {player.position}</h2>
-        <Link to='/roster'>Back</Link>
-      </div>
-    )
   }
-  
-  export default Player
+`;
+
+const VideoPlayer = styled.div`
+  height: 100vh;
+  width: calc(100% - 100px);
+`;
+
+const BackArrow = styled.button`
+  position: absolute;
+  background: none;
+  top: 40px;
+  right: 40px;
+  a {
+    color: #fff;
+  }
+`;
