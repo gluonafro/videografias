226af197342c8362376d2a983c57c9f2ec4456a7
gluonafro <gluonafro@outlook.es>
2020-07-26 15:25:54 +0200

    mobile: play video on screen

--- src/containers/CarruselMobile.js
+++ src/containers/CarruselMobile.js
@@ -4,10 +4,20 @@ import { data } from "../resources/data.json";
 import { useTranslate } from "../contexts/languageContext";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { Link } from "react-router-dom";
+import { useScrollPosition } from "../hooks/useScrollPosition";
 
 const CarruselMobile = ({ orderedData }) => {
   const t = useTranslate();
   const [items, setItems] = useState(orderedData);
+  const [active, setActive] = useState(0);
+
+  const mediaHeight = 0.5625 * window.innerWidth;
+  const itemHeight = mediaHeight + 89.5;
+  useScrollPosition(({ prevPos, currPos }) => {
+    const pos = Math.abs(currPos.y - 200);
+    let videoAct = currPos.y >= -50 ? 0 : Math.floor(pos / itemHeight);
+    setActive(videoAct);
+  }, []);
 
   return (
     <Wrapper>
@@ -15,23 +25,40 @@ const CarruselMobile = ({ orderedData }) => {
         dataLength={items.length}
         next={() => setItems(items.concat(orderedData))}
         hasMore={true}
-        height="calc(100vh - 5rem)"
       >
-        {items.map((i) => (
-          <Item>
-            <Link to={`/expo/${i}`}>
-              <img
-                width="100%"
-                height="100%"
-                src={
-                  process.env.PUBLIC_URL +
-                  "/assets/img/1920x1080/" +
-                  data[i].id +
-                  ".jpg"
-                }
-                alt={data[i].videoName}
-              ></img>
-            </Link>
+        {items.map((i, index) => (
+          <div key={index}>
+            <MediaContainer height={window.innerWidth * 0.5625}>
+              <Link to={`/expo/${i}`}>
+                {index === active ? (
+                  <video
+                    width="100%"
+                    src={data[i].preview}
+                    muted
+                    autoPlay
+                    poster={
+                      process.env.PUBLIC_URL +
+                      "/assets/img/1920x1080/" +
+                      data[i].id +
+                      ".jpg"
+                    }
+                    loop
+                  />
+                ) : (
+                  <img
+                    width="100%"
+                    height="100%"
+                    src={
+                      process.env.PUBLIC_URL +
+                      "/assets/img/1920x1080/" +
+                      data[i].id +
+                      ".jpg"
+                    }
+                    alt={data[i].videoName}
+                  ></img>
+                )}
+              </Link>
+            </MediaContainer>
             <VideoInfo>
               <p>
                 <strong>{data[i].videoName}</strong>
@@ -41,7 +68,7 @@ const CarruselMobile = ({ orderedData }) => {
                 {data[i].year} · {t(data[i].country)}
               </p>
             </VideoInfo>
-          </Item>
+          </div>
         ))}
       </InfiniteScroll>
     </Wrapper>
@@ -52,17 +79,17 @@ export default CarruselMobile;
 
 const Wrapper = styled.section`
   margin-top: 5rem;
-  div div {
-    -ms-overflow-style: none; /* IE and Edge */
-    scrollbar-width: none; /* Firefox */
-    /* Hide scrollbar for Chrome, Safari and Opera */
-    ::-webkit-scrollbar {
-      display: none;
-    }
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
+  /* Hide scrollbar for Chrome, Safari and Opera */
+  ::-webkit-scrollbar {
+    display: none;
   }
 `;
 
-const Item = styled.div``;
+const MediaContainer = styled.div`
+  height: ${({ height }) => height + "px"};
+`;
 
 const VideoInfo = styled.div`
   margin: 0.75rem 0 2.2rem 0.75rem;
