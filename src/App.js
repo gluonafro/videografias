2c92e6ad9e13e57dde8a767ee8ca08a5fa033fe1
gluonafro <gluonafro@outlook.es>
2020-07-29 20:41:18 +0200

    move fonts css to index.css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React from 'react';
-import "./styles/fonts.css";
+import React from "react";
 import GlobalStyles from "./styles/global";
 import Format from "./styles/format";
 import { ThemeProvider } from "styled-components";
-import { HashRouter } from "react-router-dom"
-import Router from "./router/Router"
+import { HashRouter } from "react-router-dom";
+import Router from "./router/Router";
 import theme from "./styles/theme";
-import { LanguageProvider, useLanguage } from "./contexts/languageContext";
+import { LanguageProvider } from "./contexts/languageContext";
 
 function App() {
   return (
