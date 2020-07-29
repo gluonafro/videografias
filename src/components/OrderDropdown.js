a387e6d42f9f332faa30dea49046274b6136927f
gluonafro <gluonafro@outlook.es>
2020-07-29 20:43:41 +0200

    delete not used files

--- src/components/OrderDropdown.js
+++ src/components/OrderDropdown.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import constants from "../resources/constants.json";
 import randomArray from "../utils/randomArray";
