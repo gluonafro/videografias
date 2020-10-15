51de0b2d7d7e2f8cba2330cac59f6a87a17f3b7f
gluonafro <gluonafro@outlook.es>
2020-10-15 14:20:03 +0200

    add data and literals

--- src/resources/literals.js
+++ src/resources/literals.js
@@ -26,6 +26,20 @@ const languages = {
     ita: "Italia",
     gec: "Guinea Ecuatorial",
     arg: "Argentina",
+    hon: "Honduras",
+    par: 'Paraguay',
+    bol: "Bolivia",
+    uru: 'Uruguay',
+    mex: 'México',
+    cri: 'Costa Rica',
+    sal: 'El Salvador',
+    chi: 'Chile',
+    rdo: 'República Dominicana',
+    pan: 'Panamá',
+    gua: 'Guatemala',
+    usa: 'EEUU',
+    nic: 'Nicaragua',
+    per: 'Perú',
     az: "(A-Z)",
     canepaBio:
       "Andrea Canepa (Lima-1980) estudió en la Universidad Politécnica de Valencia y obtuvo una Maestría en Artes Visuales y Multimedia. Ha sido becada por el Departamento de Cultura del Senado de Berlín y por la Fundación Endesa. Ha ganado los premios: ARCO para Jóvenes Artistas, Miquel Casablancas y Generaciones 2014. Ha sido artista residente en Gasworks, MATADERO, Cité des Arts, Tokyo Wonder Site, Beta Local, Bauhaus Masters Houses, Jan Van Eyck Academie y en La Academia de España en Roma.",
@@ -79,10 +93,174 @@ const languages = {
       "Artista conceptual, surge en un cierto momento, no busca ningún tipo de acercamiento seguro y prefiere, en su hábil viaje como fugitivo del concepto, una pregunta reconfortante que una respuesta discutible. Ha trabajado y ha expuesto en Cracovia, Milán, Nueva York, Nova Gorica, París, Roma, Trieste, Turín, Venecia... Ahora está aquí. Ha participado con un solo-show en la reciente Artissima con la Galería Massimo de Luca. En 2018 salió It’s Up for You firmada por ILLY ART COLLECTION.",
     attruiaVideo:
       "All I need is all I need is all I need is all… la repetición de la frase sin solución de continuidad. Todo aquello que necesito coincide con la totalidad de lo que necesito y es exactamente todo lo que necesito. No necesito dinero, no necesito amor, no necesito familia… necesito todo. Si puedo elegir, ¿por qué tendría que elegir? Elijo quedarme con todo. Un todo que quizás coincide con un nada… y entonces, ¿cuál es la elección correcta?",
+    encinaBio:
+      "",
+    encinaVideo:
+      "",
+    mastroBio:
+      "",
+    mastroVideo:
+      "",
+    sanchezBio:
+      "",
+    sanchezVideo:
+      "",
+    menschBio:
+      "",
+    menschVideo:
+      "",
+    denegriBio:
+      "",
+    denegriVideo:
+      "",
+    levyBio:
+      "",
+    levyVideo:
+      "",
+    sfercoBio:
+      "",
+    sfercoVideo:
+      "",
+    cuquiBio:
+      "",
+    cuquiVideo:
+      "",
+    videlaBio:
+      "",
+    videlaVideo:
+      "",
+    linaresBio:
+      "",
+    linaresVideo:
+      "",
+    chavajayBio:
+      "",
+    chavajayVideo:
+      "",
+    lopezBio:
+      "",
+    lopezVideo:
+      "",
+    nveBio:
+      "",
+    nveVideo:
+      "",
+    asseduBio:
+      "",
+    asseduVideo:
+      "",
+    nsueBio:
+      "",
+    nsueVideo:
+      "",
+    tejadaBio:
+      "",
+    tejadaVideo:
+      "",
+    romanBio:
+      "",
+    romanVideo:
+      "",
+    proyecto3399Bio:
+      "",
+    proyecto3399Video:
+      "",
+     ximenaBio:
+      "",
+    ximenaVideo:
+      "",
+    garciaBio:
+      "",
+    garciaVideo:
+      "",
+    tlasecaBio:
+      "",
+    tlasecaVideo:
+      "",
+    foglinoBio:
+      "",
+    foglinoVideo:
+      "",
+    castagnoBio:
+      "",
+    castagnoVideo:
+      "",
+    diazBio:
+      "",
+    diazVideo:
+      "",
+    paguagaBio:
+      "",
+    paguagaVideo:
+      "",
+    mgarciaBio:
+      "",
+    mgarciaVideo:
+      "",
+    guerraBio:
+      "",
+    guerraVideo:
+      "",
+    vegaBio:
+      "",
+    vegaVideo:
+      "",
+    laojimenezBio:
+      "",
+    laojimenezVideo:
+      "",
+    retamalastudilloBio:
+      "",
+    retamalastudilloVideo:
+      "",
+    belarminoBio:
+      "",
+    belarminoVideo:
+      "",
+    rupcichBio:
+      "",
+    rupcichVideo:
+      "",
+    alcantaraBio:
+      "",
+    alcantaraVideo:
+      "",
+    jimenezBio:
+      "",
+    jimenezVideo:
+      "",
+    karmadavisBio:
+      "",
+    karmadavisVideo:
+      "",
+    ochoaBio:
+      "",
+    ochoaVideo:
+      "",
+    leivaBio:
+      "",
+    leivaVideo:
+      "",
+    vallejoBio:
+      "",
+    vallejoVideo:
+      "",
+    romanoBio:
+      "",
+    romanoVideo:
+      "",
+    clottiBio:
+      "",
+    clottiVideo:
+      "",
+    desuqueBio:
+      "",
+    desuqueVideo:
+      "",
     sadaba00:
       "Son ya más de cincuenta años desde que el arte del vídeo apareció por vez primera en el entorno del arte, y cuarenta de cuando el contexto del arte español empezó tímidamente a mostrar interés por unas obras siempre relacionadas de forma exótica con la innovación y aquello que de forma difusa se llamaban “nuevas tecnologías”. No será hasta finales de los años noventa cuando las obras de vídeo comenzarán a verse con cierta normalidad en galerías y museos, inmersas en el proceso de audiovisualización generalizada de nuestras formas de vida: las cada vez mayores posibilidades de acceso por parte de los y las artistas a las formas de grabación y procesado de las imágenes, de un lado, y la también cada vez más asequible posibilidad de proyección de las mismas, por otro, son algunas de las razones que contribuyeron a facilitar este proceso. Sin embargo el mundo del arte no siempre ha respondido con la misma inmediatez a estas nuevas formas de producción y exhibición de las y los artistas: aún hoy resulta común ver las proyecciones de los trabajos en vídeo en contextos escindidos de los de otras formas de creación, mientras que paradójicamente el vídeo como soporte narrativo se nos aparece de manera cotidiana en todos los momentos del día. Con la idea de paliar en parte este déficit, y poder así visibilizar en diferentes contextos expositivos este tipo de trabajos, la Academia de España en Roma presenta en esta ocasión una selección de trabajos videográficos de algunos de los artistas que han pasado por sus instalaciones, de tal modo que en el dispositivo ideado para ello las diferentes obras se retroalimenten entre sí. Frecuentemente se ha hablado también del problema de la adecuación temporal de los trabajos de base tecnológica y/o audiovisual (Time Based Arts) a los dispositivos expositivos propios de la sala o galería, estáticos por definición. Es por ello también por lo que hemos hecho un esfuerzo por tomar en consideración una serie de obras cuya concepción narrativa se adapte a las características exhibitivas de la sala de exposición. Tras esta primera experiencia romana, el AECID quiere mantener este impulso más allá, para llevar a conocer estos y otros trabajos a otras sedes amigas: por ello habrá una serie más extensa de obras circulando durante los dos próximos años (2020, 2021) por diversas ciudades iberoamericanas. Tanto en esta primera presentación en Roma, como en las otras ciudades por las que circulen estas muestras, se invitará a comisarios/as locales para que seleccionen algunos trabajos procedentes de sus propias ciudades que puedan dialogar con las obras seleccionadas de los/as artistas de la Academia.<br>Esta selección se complementa en este arranque de proyecto con la obra de tres artistas comisariada por Anita Calá en el que veremos los trabajos de artistas como Francesca Arri, Matteo Attruia y Lamberto Teotino, será muy interesante ver como conviven y dialogan las obras de estos seis artistas en La Academia de España en Roma.",
     cala01:
-      "<p>La simetría acompaña a menudo la idea de una visión equilibrada y armónica en una composición figurativa. Utilizada sobretodo en la Pintura Medieval, conseguía dar un sentido de estatismo, inmovilizando la escena retratada en una eterna instantánea, pero al mismo tiempo equilibrando el ritmo visual de la misma. Este paralelismo estético nace cuando una o más formas colocadas en el espacio presentan repeticiones regulares, siempre con un eje en el centro del que irradian elementos más o menos especulares.</p><br /><p>En contraposición, en el lenguaje artístico contemporáneo, existe una búsqueda obsesiva por desestabilizar la serenidad que generan las imágenes reconfortantes de lo cotidiano, explorando las dinámicas generadas a partir de la descomposición de una estructura reconocible.</p><br /><p>En el 1985 Peter Greenaway realiza el “El zoo”, una obra cinematográfica en la que consigue provocar un cortocircuito en la percepción del observador, contrastando la simetría rígida y reconfortante de las imágenes con una historia surreal y llena de movimiento creando un colapso visual, a través de formas y colores que se descomponen en un <em> loop </em> sincopado. Y es precisamente del desafío de jugar con dos lenguajes distintos, simetría y caos, que nace la idea de confrontar a los tres artistas elegidos: Francesca Arri, Matteo Attruia e Lamberto Teotino. Sus obras giran entorno a un eje central, tanto en el plano estructural como el plano visual.</p><br />      <p>En el video-performance <em>Self Portrait</em> di <strong> Francesca Arri </strong> (2012) es la carne: el cuerpo, situado inicialmente como el núcleo, desnudo y solo, en el centro y bien visible en su totalidad, pierde lentamente la propia unicidad y fragilidad cuando el observador, que sufre la potencia visual de la imagen a medida que la observa, dándose cuenta de que el objetivo es él mismo, puesto en medio, ridiculizado, observado y burlado por ese mismo cuerpo, que al principio divierte y cautiva, y después avergüenza hasta llegar a molestar de un modo grotesco al ojo que lo observa.</p><br /><p><strong> Matteo Attruia </strong> presenta la obra inédita <em> All I Need Is All I Need Is All </em> (2020), un video alienante que nos reta con el instrumento de la palabra: se trata de una boca en primer plano, que con una voz sintética nos hipnotiza a través de la repetición de una frase sin solución de continuidad, haciéndonos entrar en el loop de la búsqueda y de la satisfacción de nuestros deseos, donde todo aquello que necesito coincide exactamente con la totalidad de lo que necesito, que es todo aquello que necesito…</p><br /><p><strong> Lamberto Teotino </strong> desestructura el objeto en <em> non-physical entities </em> (2018-2020), da a luz nuevas formas inexistentes, suscita curiosidad y nos empuja a analizar sus reelaboraciones de la estética primordial: criaturas metálicas suspendidas en un limbo amniótico hecho de aire, vibran en un juego caleidoscópico con una simetría delirante.</p><br /><p>Los tres artistas utilizan el video como medio para crear un tejido de lenguajes que van más allá de la simple observación de la imagen. Envuelven al espectador en una danza de sentidos emocionales, excavando en nuestros recuerdos en busca del por qué. Detrás de estas composiciones de fácil lectura, encontramos esa sensación de malestar que sólo el instinto percibe, haciendo atractivo lo que en la cotidianeidad rechaza.</p>",
+      "<p>La simetría acompaña a menudo la idea de una visión equilibrada y armónica en una composición figurativa. Utilizada sobretodo en la Pintura Medieval, conseguía dar un sentido de estatismo, inmovilizando la escena retratada en una eterna instantánea, pero al mismo tiempo equilibrando el ritmo visual de la misma. Este paralelismo estético nace cuando una o más formas colocadas en el espacio presentan repeticiones regulares, siempre con un eje en el centro del que irradian elementos más o menos especulares.</p><br /><p>En contraposición, en el lenguaje artístico contemporáneo, existe una búsqueda obsesiva por desestabilizar la serenidad que generan las imágenes reconfortantes de lo cotidiano, explorando las dinámicas generadas a partir de la descomposición de una estructura reconocible.</p><br /><p>En el 1985 Peter Greenaway realiza el “El zoo”, una obra cinematográfica en la que consigue provocar un cortocircuito en la percepción del observador, contrastando la simetría rígida y reconfortante de las imágenes con una historia surreal y llena de movimiento creando un colapso visual, a través de formas y colores que se descomponen en un <em> loop </em> sincopado. Y es precisamente del desafío de jugar con dos lenguajes distintos, simetría y caos, que nace la idea de confrontar a los tres artistas elegidos: Francesca Arri, Matteo Attruia e Lamberto Teotino. Sus obras giran entorno a un eje central, tanto en el plano estructural como el plano visual.</p><br />      <p>En el video-performance <em>Self Portrait</em> di <strong> Francesca Arri </strong> (2012) es la carne: el cuerpo, situado inicialmente como el núcleo, desnudo y solo, en el centro y bien visible en su totalidad, pierde lentamente la propia unicidad y fragilidad cuando el observador, que sufre la potencia visual de la imagen a medida que la observa, dándose cuenta de que el objetivo es él mismo, puesto en medio, ridiculizado, observado y burlado por ese mismo cuerpo, que al principio divierte y cautiva, y después avergüenza hasta llegar a molestar de un modo grotesco al ojo que lo observa.</p><br /><p><strong> Matteo Attruia </strong> presenta la obra inédita <em> All I Need Is All I Need Is All </em> (2020), un video alienante que nos reta con el instrumento de la palabra: se trata de una boca en primer plano, que con una voz sintética nos hipnotiza a través de la repetición de una frase sin solución de continuidad, haciéndonos entrar en el loop de la búsqueda y de la satisfacción de nuestros deseos, donde todo aquello que necesito coincide exactamente con la totalidad de lo que necesito, que es todo aquello que necesito…</p><br /><p><strong> Lamberto Teotino </strong> desestructura el objeto en <em> non-physical entities </em> (2018-2020), da a luz nuevas formas inexistentes, suscita curiosidad y nos empuja a analizar sus reelaboraciones de la estética primordial: criaturas metálicas suspendidas en un limbo amniótico hecho de aire, vibran en un juego caleidoscópico con una simetría delirante.</p><br /><p>Los tres artistas utilizan el video como medio para crear un tejido de lenguajes que van más allá de la simple observación de la imagen. Envuelven al espectador en una danza de sentidos emocionales, excavando en nuestros recuerdos en busca del por qué. Detrás de estas composiciones de fácil lectura, encontramos esa sensación de malestar que sólo el instinto percibe, haciendo atractivo lo que en la cotidianeidad rechaza.</p>",  
   },
   it: {
     informacion: "",
