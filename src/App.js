bd718a042e2c807f39cbd40481d4231c61faea07
gluonafro <gluonafro@outlook.es>
2020-10-24 18:33:44 +0200

    save password in localStorage

--- src/App.js
+++ src/App.js
@@ -6,41 +6,42 @@ import { HashRouter } from "react-router-dom";
 import Router from "./router/Router";
 import theme from "./styles/theme";
 import { LanguageProvider } from "./contexts/languageContext";
+import useLocalStorage from "./hooks/useLocalStorage";
 
 function App() {
-  const [password, setPassword] = React.useState("");
+  const [password, setPassword] = useLocalStorage("password", "");
   const pass = "paips";
   return (
     <>
       {password === pass ? (
-    <ThemeProvider theme={theme}>
-      <HashRouter>
-        <LanguageProvider>
-          <GlobalStyles />
-          <Format />
-          <Router />
-        </LanguageProvider>
-      </HashRouter>
-    </ThemeProvider>
-    ) : (
-      <div
-        style={{
-          position: "absolute",
-          left: "calc(50% - 100px)",
-          top: "100px",
-          width: "200px",
-        }}
-      >
-        <div>Introduce la contraseña</div>
-        <textarea
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          style={{ height: "15px", marginTop: "10px" }}
-          autoFocus
-        ></textarea>
-      </div>
-    )}
-  </>
+        <ThemeProvider theme={theme}>
+          <HashRouter>
+            <LanguageProvider>
+              <GlobalStyles />
+              <Format />
+              <Router />
+            </LanguageProvider>
+          </HashRouter>
+        </ThemeProvider>
+      ) : (
+        <div
+          style={{
+            position: "absolute",
+            left: "calc(50% - 100px)",
+            top: "100px",
+            width: "200px",
+          }}
+        >
+          <div>Introduce la contraseña</div>
+          <textarea
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ height: "15px", marginTop: "10px" }}
+            autoFocus
+          ></textarea>
+        </div>
+      )}
+    </>
   );
 }
 
